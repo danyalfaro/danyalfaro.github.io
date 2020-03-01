34dd8760022b2dac7476e79794d2ffc0abe703e2
Daniel Alfaro R <daniel.alfaro@upr.edu>
2020-03-01 14:39:29 -0400

    Modified nav bar, added real bio content.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+  if (document.body.scrollTop > 15 || document.documentElement.scrollTop > 15) {
     document.getElementById("rec1").className = "rec animation1";
     document.getElementById("rec2").className = "rec animation2";
     document.getElementById("rec3").className = "rec animation3";

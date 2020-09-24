0d3c6e3d89a47478e691174e38d98385c9142955
Daniel Alfaro R <daniel.alfaro@upr.edu>
2020-09-24 13:52:33 -0400

    added closing transform for mobile menu

--- mobileNav.js
+++ mobileNav.js
@@ -1,5 +1,7 @@
 const menuLinks = document.getElementById("menuLinks");
+const burger = document.getElementById("burger");
 document.getElementById("burger").addEventListener("click", () => {
   console.log("CLICKEDDDD!");
   menuLinks.classList.toggle("burgerActive");
+  burger.classList.toggle("burgerClose");
 });

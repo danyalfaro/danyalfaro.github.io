c06970738b0d67f7b36174a396d35f86cdeb6a6d
Daniel Alfaro R <daniel.alfaro@upr.edu>
2020-10-29 23:01:38 -0400

    Overall mobile experience improvements.

--- mobileNav.js
+++ mobileNav.js
@@ -1,7 +1,6 @@
 const menuLinks = document.getElementById("menuLinks");
 const burger = document.getElementById("burger");
 document.getElementById("burger").addEventListener("click", () => {
-  console.log("CLICKEDDDD!");
   menuLinks.classList.toggle("burgerActive");
   burger.classList.toggle("burgerClose");
 });
